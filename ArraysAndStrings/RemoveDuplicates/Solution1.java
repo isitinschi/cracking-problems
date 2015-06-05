b5990760815210d45bb6a783fb6cb0c5f15ea7bc
iuri <iuri@fake.com>
2015-06-05 10:01:03 +0300

    remove duplicates problem

---
+++ ArraysAndStrings/RemoveDuplicates/Solution1.java
@@ -0,0 +1,26 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution1 {
+	
+	public static void main(String... args) {
+		if (args.length != 1) {
+			throw new IllegalArgumentException("Should be one input string parameter");
+		}
+		
+		String input = args[0];
+		
+		for (int i = 0; i < input.length(); ++i) {
+			for (int j = i + 1; j < input.length(); ++j) {
+				if (input.charAt(i) == input.charAt(j)) {
+					input = input.replaceAll(input.substring(i, i + 1), "");
+					--i;
+					break;
+				}
+			}
+		}
+		
+		System.out.println("Result string: " + input);
+	}
+	
+}
